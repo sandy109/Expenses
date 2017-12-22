3e57e1be8b8dd3309de7df2802537f2d19b21003
Nijraj Gelani <gelaninijraj@gmail.com>
2017-12-22 22:13:32 +0000

    Misc changes

--- app/src/main/java/me/nijraj/expesnses/adapters/DashboardAdapter.java
+++ app/src/main/java/me/nijraj/expesnses/adapters/DashboardAdapter.java
@@ -1,7 +1,9 @@
 package me.nijraj.expesnses.adapters;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,8 @@ import java.util.List;
 
 import me.nijraj.expesnses.R;
 import me.nijraj.expesnses.fragments.FragmentAddExpense;
+import me.nijraj.expesnses.fragments.FragmentExpenses;
+import me.nijraj.expesnses.models.Expense;
 
 /**
  * Created by buddha on 12/15/17.
@@ -21,18 +25,34 @@ import me.nijraj.expesnses.fragments.FragmentAddExpense;
 public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.ViewHolder> {
 
     List<DashboardItem> data;
+    FragmentManager fragmentManager;
 
     public DashboardAdapter(List<DashboardItem> data) {
+        this.data = data;
+    }
+
+    public DashboardAdapter(FragmentManager fragmentManager, List<DashboardItem> data) {
+        this.fragmentManager = fragmentManager;
         this.data = data;
     }
 
     public static class DashboardItem{
         private String title;
         private double amount;
+        private Expense.TYPE type;
 
-        public DashboardItem(String title, double amount) {
+        public DashboardItem(String title, Expense.TYPE type) {
             this.title = title;
-            this.amount = amount;
+            this.amount = Expense.getTotalOfType(type);
+            this.type = type;
+        }
+
+        public Expense.TYPE getType() {
+            return type;
+        }
+
+        public void setType(Expense.TYPE type) {
+            this.type = type;
         }
 
         public String getTitle() {
@@ -52,14 +72,32 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.View
         }
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder{
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-        public TextView textViewName, textViewAmount;
+        TextView textViewName, textViewAmount;
+        CardView cardView;
+        DashboardAdapter adapter;
 
         ViewHolder(View itemView) {
             super(itemView);
             textViewName = itemView.findViewById(R.id.textView_name);
             textViewAmount = itemView.findViewById(R.id.textView_amount);
+            cardView = itemView.findViewById(R.id.cardView);
+            cardView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            FragmentExpenses expenses = new FragmentExpenses();
+            Bundle args = new Bundle();
+            args.putSerializable("type", adapter.data.get(getAdapterPosition()).getType());
+            expenses.setArguments(args);
+            FragmentTransaction fragmentTransaction = adapter.fragmentManager.beginTransaction();
+            fragmentTransaction
+                    .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                    .replace(R.id.fragment_container, expenses)
+                    .addToBackStack("add_expense")
+                    .commit();
         }
     }
 
@@ -72,6 +110,7 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.View
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.textViewName.setText(data.get(position).getTitle());
+        holder.adapter = this;
         holder.textViewAmount.setText("₹" + data.get(position).getAmount());
     }
 

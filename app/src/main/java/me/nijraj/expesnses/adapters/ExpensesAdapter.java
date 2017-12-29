f5af028c4065596b32fa267d0e835b1fb8c63bb3
Nijraj Gelani <gelaninijraj@gmail.com>
2017-12-29 20:20:25 +0000

    Added dialog on long press and multi select functionality

--- app/src/main/java/me/nijraj/expesnses/adapters/ExpensesAdapter.java
+++ app/src/main/java/me/nijraj/expesnses/adapters/ExpensesAdapter.java
@@ -1,16 +1,20 @@
 package me.nijraj.expesnses.adapters;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -35,9 +39,11 @@ public class ExpensesAdapter extends RecyclerView.Adapter<ExpensesAdapter.ViewHo
         this.data = data;
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder{
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener, View.OnClickListener{
         TextView textViewDescription, textViewAmount, textViewDate, textViewType;
         LinearLayout linearLayout;
+        ExpensesAdapter adapter;
+
         ViewHolder(View itemView) {
             super(itemView);
             textViewDescription = itemView.findViewById(R.id.textView_description);
@@ -45,6 +51,33 @@ public class ExpensesAdapter extends RecyclerView.Adapter<ExpensesAdapter.ViewHo
             textViewDate = itemView.findViewById(R.id.textView_date);
             textViewType = itemView.findViewById(R.id.textView_type);
             linearLayout = itemView.findViewById(R.id.linearLayout_container);
+            linearLayout.setOnLongClickListener(this);
+            linearLayout.setOnClickListener(this);
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            CharSequence[] menuItems = new CharSequence[] { "Select", "Delete" };
+            AlertDialog.Builder builder = new AlertDialog.Builder(itemView.getContext());
+            builder.setTitle("Choose an action");
+            builder.setItems(menuItems, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    switch (i){
+                        case 0:
+
+                            break;
+                    }
+                }
+            });
+            builder.show();
+            return true;
+        }
+
+        @Override
+        public void onClick(View view) {
+            adapter.data.get(getAdapterPosition()).toggleSelected();
+            adapter.notifyDataSetChanged();
         }
     }
 
@@ -58,7 +91,7 @@ public class ExpensesAdapter extends RecyclerView.Adapter<ExpensesAdapter.ViewHo
     public void onBindViewHolder(ViewHolder holder, int position) {
         Expense e = data.get(position);
         String type = "";
-        switch (e.getType()){
+        switch (e.getType()) {
             case LENT:
                 type = "Lent";
                 holder.linearLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.colorGreenTransparent));
@@ -76,11 +109,23 @@ public class ExpensesAdapter extends RecyclerView.Adapter<ExpensesAdapter.ViewHo
                 holder.linearLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.colorGreenTransparent));
                 break;
         }
+        if(e.isSelected())
+            holder.linearLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.colorSelected));
         holder.textViewDescription.setText(e.getDescription());
         holder.textViewAmount.setText("₹" + e.getAmount());
         holder.textViewType.setText(type);
+        holder.adapter = this;
         Date date = new Date(e.getTimestamp());
         holder.textViewDate.setText(date.toString());
+    }
+
+    public List<Expense> getSelectedItems(){
+        ArrayList<Expense> expenses = new ArrayList<>();
+        for(Expense e: data){
+            if(e.isSelected())
+                expenses.add(e);
+        }
+        return expenses;
     }
 
     @Override

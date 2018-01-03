587b58b4a0cb5aa8370259afaf89119b8f0f13ac
Nijraj Gelani <gelaninijraj@gmail.com>
2018-01-03 20:07:39 +0000

    Added simple arithmetic calculator

--- app/src/main/java/me/nijraj/expenses/fragments/FragmentAddExpense.java
+++ app/src/main/java/me/nijraj/expenses/fragments/FragmentAddExpense.java
@@ -1,9 +1,12 @@
 package me.nijraj.expenses.fragments;
 
 import java.util.Calendar;
+import java.util.Locale;
+
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,22 +16,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import me.nijraj.expenses.R;
 import me.nijraj.expenses.models.Expense;
 import me.nijraj.expenses.models.Person;
+import me.nijraj.expenses.utils.ArithmeticParser;
 
 /**
  * Created by buddha on 12/15/17.
  */
 
-public class FragmentAddExpense extends Fragment {
+public class FragmentAddExpense extends Fragment implements View.OnClickListener {
     private Expense.TYPE type;
     private long personId;
     private EditText editTextAmount, editTextDescription;
     private Person person;
+    private LinearLayout linearLayoutCalc;
     private InputMethodManager inputMethodManager;
 
     @Nullable
@@ -45,9 +51,19 @@ public class FragmentAddExpense extends Fragment {
         inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 
+        linearLayoutCalc = view.findViewById(R.id.linearLayout_calc);
+
         type = (Expense.TYPE) getArguments().getSerializable("type");
         personId = getArguments().getLong("person");
         person = Person.findById(Person.class, personId);
+
+        // Calculator buttons
+        view.findViewById(R.id.button_add).setOnClickListener(this);
+        view.findViewById(R.id.button_subtract).setOnClickListener(this);
+        view.findViewById(R.id.button_multiply).setOnClickListener(this);
+        view.findViewById(R.id.button_divide).setOnClickListener(this);
+        view.findViewById(R.id.button_equal).setOnClickListener(this);
+
         TextView textViewDetails = view.findViewById(R.id.textView_details);
         textViewDetails.setText(getDetailsText());
         return view;
@@ -83,7 +99,7 @@ public class FragmentAddExpense extends Fragment {
 
     private void addExpense() {
         try {
-            double amount = Double.parseDouble(editTextAmount.getText().toString());
+            double amount = ArithmeticParser.eval(editTextAmount.getText().toString());
             String description = editTextDescription.getText().toString().trim();
             long timestamp = Calendar.getInstance().getTimeInMillis();
 
@@ -102,12 +118,43 @@ public class FragmentAddExpense extends Fragment {
             inputMethodManager.hideSoftInputFromWindow(getView().getWindowToken(), 0);
             getFragmentManager().popBackStack();
         }catch (NumberFormatException e){
-            Toast.makeText(getContext(), "Enter a valid amount", Toast.LENGTH_SHORT).show();
+            Snackbar.make(getView(), "Enter a valid amount", Snackbar.LENGTH_LONG).show();
+        } catch (RuntimeException e){
+            Snackbar.make(getView(), R.string.parsing_error, Snackbar.LENGTH_LONG).show();
         }
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.main, menu);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String amount = editTextAmount.getText().toString();
+        String operator = "";
+        switch (view.getId()){
+            case R.id.button_add:
+                operator = "+"; break;
+            case R.id.button_multiply:
+                operator = "*"; break;
+            case R.id.button_divide:
+                operator = "/"; break;
+            case R.id.button_subtract:
+                operator = "-"; break;
+            case R.id.button_equal:
+                try {
+                    amount = String.format(Locale.ENGLISH, "%.2f", ArithmeticParser.eval(amount));
+                }catch (RuntimeException e){
+                    Snackbar.make(getView(), R.string.parsing_error, Snackbar.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                return;
+        }
+        if(operator.length() > 0)
+            amount += operator;
+        editTextAmount.setText(amount);
+        editTextAmount.setSelection(amount.length());
     }
 }
